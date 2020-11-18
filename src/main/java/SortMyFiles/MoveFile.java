7740e15b42ee145bb452eebecf8d92727bd31109
Dakota <dakota.crozier@outlook.com>
2020-11-17 21:16:40 -0800

    added some stub functions

--- src/main/java/SortMyFiles/MoveFile.java
+++ src/main/java/SortMyFiles/MoveFile.java
@@ -1,4 +1,25 @@
 package SortMyFiles;
 
 public class MoveFile {
+
+    //this function will move a single file from one directory to another
+    public boolean moveFile(String currentPath,String name,String newPath){
+
+        return true;
+    }
+
+    //this function will move a list of files from one directory to another
+    //return true if all files were moved succesfully
+    //return false if at least one fails
+    public boolean moveFiles(String currentPath,String[] names, String newPath){
+
+        return true;
+    }
+    //will recursively move a directory
+    public boolean moveDirectory(String currentDirectory, String newPath){
+
+        return true;
+    }
+
+
 }
