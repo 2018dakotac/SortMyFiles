355265033edb913ffdebc9eeac5182e231c0c740
hideonbushlol <divs2018@gmail.com>
2020-11-26 17:15:45 -0800

    Added a basic window GUI opener and setup the JavaFx Environment

--- src/main/java/SortMyFiles/Main.java
+++ src/main/java/SortMyFiles/Main.java
@@ -1,8 +1,23 @@
 package SortMyFiles;
 
-public class Main  {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
+        launch(args);   //Sets program as JavaFx and initates start
         System.out.println("hellow world");
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Title");
+        stage.setWidth(300);
+        stage.setHeight(300);
+
+
+        stage.show();
+
+    }
 }
