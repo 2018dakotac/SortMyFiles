c3ca844c6b39d3deb9261c6477e6a3c257a67b11
Dakota Crozier <dakotac@sfu.ca>
2020-12-06 13:33:06 -0800

    remade with Files.walk

--- src/main/java/SortMyFiles/DirectoryFunctions.java
+++ src/main/java/SortMyFiles/DirectoryFunctions.java
@@ -2,11 +2,13 @@ package SortMyFiles;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.*;
 
 
 public class DirectoryFunctions {
     private FileDatabase db;
+
     public DirectoryFunctions(){
         db = new FileDatabase();
     }
@@ -33,7 +35,8 @@ public class DirectoryFunctions {
             Path target = Paths.get(pathToMake);
             Files.createDirectories(target);
         } catch (IOException e) {
-            System.err.println("Failed to make directory" + e.getMessage());
+            e.printStackTrace();
+            //System.err.println("Failed to make directory" + e.getMessage());
         }
     }
 
@@ -46,89 +49,167 @@ public class DirectoryFunctions {
 
 
 
-    //will manually move a directory and its contents and update the database
-    public boolean moveDirectory(String currentPath, String newPath) {
+    //will move the entire directory including the folder itself
+    public void moveDirectory(String currentPath, String newPath) throws IOException {
         File from = new File(currentPath);
         //File to = new File(newPath); this moves directory contents to new location
         File to = new File(combine(newPath,from.getName()));
 
-        return recursiveMoveDir(from.toPath(), to.toPath());
+        recursiveMoveDir(from.toPath(), to.toPath());
     }
-    //recursive moving function to allow moving of recursive directories
-    private boolean recursiveMoveDir(Path source, Path destination){
-        if (source.toFile().isDirectory()) {
-            //iterates through the list of files returned  by Files.listFiles
-            for (File file : source.toFile().listFiles()) {// NULLPOINTER EXCEPTION CHECK ???????????????????????????????????????????????
-                recursiveMoveDir(file.toPath(), destination.resolve(source.relativize(file.toPath())));
-            }
-        }else{//updates directory for non folder files if they exist within the database
-            db.updateFile(source.toString(), destination.normalize().toString());
-        }
+    //will  move a directory's content
+    public void moveDirectoryContents(String currentPath, String newPath) throws IOException {
+        File from = new File(currentPath);
+        //File to = new File(newPath); this moves directory contents to new location
+        File to = new File(newPath);
 
-        try {
-            Files.move(source, destination, StandardCopyOption.REPLACE_EXISTING);
-            //standard copy operation makes this not fail if used on own directory
-
-            return true;
-        } catch (IOException e) {
-            return false;
-        }
-
+        recursiveMoveDir(from.toPath(), to.toPath());
     }
-    public void deleteDirectory(File file) throws IOException {
-        if (file.isDirectory()) {
-            File[] entries = file.listFiles();
-            if (entries != null) {
-                for (File entry : entries) {
-                    deleteDirectory(entry);
-                }
-            }
-        }else{
-            //deletes from database
-            db.deleteFile(file.getAbsolutePath());
-            //System.out.println(file.getAbsolutePath());
-        }
-        if (!file.delete()) {
-            throw new IOException("Failed to delete " + file);
-        }
-    }
-
-
-    public void copyDirectory(String srcDir, String destDir)
-            throws IOException {
-        Files.walk(Paths.get(srcDir))
-                .forEach(source -> {
-                    Path destination = Paths.get(destDir, source.toString()
-                            .substring(srcDir.length()));
-                    try {
-                        Files.copy(source, destination);
-
-                    } catch (IOException e) {
+    //https://stackoverflow.com/questions/34820734/java-moving-files-within-filesystem
+    private void recursiveMoveDir(Path source, Path destination) throws IOException{
+        Files.createDirectories(destination);
+        Files.walk(source)
+                .filter(Files::isDirectory)
+                .forEach(path -> {
+                    try{
+                        Files.createDirectories(path);
+                    }catch(IOException e){
                         e.printStackTrace();
+                        throw new UncheckedIOException(e);
                     }
+
+                });
+        Files.walk(source)
+                .filter(Files::isRegularFile)
+                .forEach(path -> {
+                    Path dest = destination.resolve(source.relativize(path));
+                    try{
+                        Files.createDirectories(dest.getParent());
+                        //Files.move(path,dest,StandardCopyOption.REPLACE_EXISTING);
+                        Files.move(path,dest);
+                        db.updateFile(path.toAbsolutePath().normalize().toString(), dest.toAbsolutePath().normalize().toString());
+                    }catch(IOException e){
+                        e.printStackTrace();
+                        throw new UncheckedIOException(e);
+                    }
+
                 });
     }
 
-    //moves a directory but doesnt update the database
-    //needs some error checking like if the directory already exists
-    public boolean moveDirectoryNoUpdate(String currentPath, String newPath) {
 
-        File from = new File(currentPath);
+    /*
+    //recursive moving function to allow moving of recursive directories
+    private void recursiveMoveDir(Path source, Path destination) throws IOException {
+        if (source.toFile().isDirectory()) {
+            //iterates through the list of files returned  by Files.listFiles
+            for (File file : source.toFile().listFiles()) {// NULLPOINTER EXCEPTION CHECK ?????????????????
+                recursiveMoveDir(file.toPath(), destination.resolve(source.relativize(file.toPath())));
+            }
+        }else{//updates directory for non folder files if they exist within the database
 
-        File to = new File(combine(newPath,from.getName()));
-        //this will result in moving an exact copy of the directory including the folder itself
-        //as a sub folder to the location folder
-
-        try {
-            Files.move(from.toPath(), to.toPath(), StandardCopyOption.REPLACE_EXISTING);
-        } catch(DirectoryNotEmptyException e){
-            e.printStackTrace();
+            //db.updateFile(source.toRealPath().toString(),destination.toRealPath().toString());
+            //while real path is better it requires both of them to exist on the disk at the same time which isnt possible.
+            db.updateFile(source.toAbsolutePath().normalize().toString(), destination.toAbsolutePath().normalize().toString());
         }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        return true;
+
+            Files.move(source, destination, StandardCopyOption.REPLACE_EXISTING);
+            //standard copy operation makes this not fail if used on own directory
+
     }
+
+     */
+
+    public void deleteDirectory(String path) throws IOException{
+        recursiveDelDir(new File(path));
+    }
+    private void recursiveDelDir(File file) throws IOException {
+        if(file.exists()) {
+            if (file.isDirectory()) {
+                File[] entries = file.listFiles();
+                if (entries != null) {
+                    for (File entry : entries) {
+                        recursiveDelDir(entry);
+                    }
+                }
+            } else {
+                //deletes from database
+                db.deleteFile(file.getCanonicalPath());
+                //System.out.println(file.getAbsolutePath());
+            }
+            if (!file.delete()) {
+                throw new IOException("Failed to delete " + file);
+            }
+        }
+    }
+
+    //will copy a directory itself and its contents and update the database
+    public void copyDirectory(String currentPath, String newPath) throws IOException {
+        File from = new File(currentPath);
+        //File to = new File(newPath); this moves directory contents to new location
+        File to = new File(combine(newPath,from.getName()));
+
+         recursiveCopyDir(from.toPath(), to.toPath());
+    }
+
+    //will only copy the contents of specified directory
+    public void copyDirectoryContents(String currentPath, String newPath) throws IOException {
+        File from = new File(currentPath);
+        //File to = new File(newPath); this moves directory contents to new location
+        File to = new File(newPath);
+
+         recursiveCopyDir(from.toPath(), to.toPath());
+    }
+    /*
+    //recursive moving function to allow moving of recursive directories
+    private void recursiveCopyDir(Path source, Path destination) throws IOException {
+        if (source.toFile().isDirectory()) {
+            //iterates through the list of files returned  by Files.listFiles
+            for (File file : source.toFile().listFiles()) {// NULLPOINTER EXCEPTION CHECK ?????????????????
+                recursiveMoveDir(file.toPath(), destination.resolve(source.relativize(file.toPath())));
+            }
+        }
+            Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+            db.copyFile(source.toRealPath().toString(),destination.toRealPath().toString());
+            //standard copy operation makes this not fail if used on own directory
+    }
+
+     */
+    private void recursiveCopyDir(Path source, Path destination) throws IOException{
+        Files.createDirectories(destination);
+        Files.walk(source)
+                .filter(Files::isDirectory)
+                .forEach(path -> {
+                    try{
+                        Files.createDirectories(path);
+                    }catch(IOException e){
+                        e.printStackTrace();
+                        throw new UncheckedIOException(e);
+                    }
+
+                });
+        Files.walk(source)
+                .filter(Files::isRegularFile)
+                .forEach(path -> {
+                    Path dest = destination.resolve(source.relativize(path));
+                    try{
+                        Files.createDirectories(dest.getParent());
+                        //Files.copy(path,dest,StandardCopyOption.REPLACE_EXISTING);
+                        Files.copy(path,dest);
+                        db.copyFile(path.toAbsolutePath().normalize().toString(), dest.toAbsolutePath().normalize().toString());
+                    }catch(IOException e){
+                        e.printStackTrace();
+                        throw new UncheckedIOException(e);
+                    }
+
+                });
+    }
+
+    public boolean renameDirectory(String currentPath,String newPath){
+       return false;
+    }
+
+
+
     /*
     //untested just incase deleteDirectory1 isnt suitable
     public void deleteDirectoryBackup(Path path) throws IOException {
@@ -145,4 +226,5 @@ public class DirectoryFunctions {
         db.deleteFile(path.toAbsolutePath().toString());
     }
      */
+
 }
