6255087f809a482c4b2d6cb3a6e7c67d21e84f80
Dakota Crozier <dakotac@sfu.ca>
2020-12-10 15:05:32 -0800

    added dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module SortMyFiles {
     requires javafx.fxml;
 
     requires java.sql;
+    requires org.apache.commons.io;
 
     opens SortMyFiles to javafx.fxml;
     opens SortMyFiles.Controllers;
